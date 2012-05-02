f4bbe735453f352b1ae54739b8f7936bcc9f3db9
Julien Carsique <jcarsique@nuxeo.com>
2012-05-02 12:33:23 +0200

    add objective constants

--- src/main/java/org/eclipse/equinox/p2/cudf/solver/SolverConfiguration.java
+++ src/main/java/org/eclipse/equinox/p2/cudf/solver/SolverConfiguration.java
@@ -1,23 +1,33 @@
 package org.eclipse.equinox.p2.cudf.solver;
 
 public class SolverConfiguration {
-	public boolean verbose = false;
-	public String objective = "p2";
-	public String timeout = "default";
-	public boolean explain = false;
-	public boolean encoding = false;
+    public static final String OBJ_PARANOID = "paranoid";
 
-	public SolverConfiguration(String objective, String timeout, boolean verbose, boolean explain, boolean encoding) {
-		if (objective != null)
-			this.objective = objective;
-		if (timeout != null)
-			this.timeout = timeout;
-		this.verbose = verbose;
-		this.explain = explain;
-		this.encoding = encoding;
-	}
+    public static final String OBJ_TRENDY = "trendy";
 
-	public SolverConfiguration(String objective, String timeout, boolean verbose, boolean explain) {
-		this(objective, timeout, verbose, explain, false);
-	}
+    public boolean verbose = false;
+
+    public String objective = "p2";
+
+    public String timeout = "default";
+
+    public boolean explain = false;
+
+    public boolean encoding = false;
+
+    public SolverConfiguration(String objective, String timeout,
+            boolean verbose, boolean explain, boolean encoding) {
+        if (objective != null)
+            this.objective = objective;
+        if (timeout != null)
+            this.timeout = timeout;
+        this.verbose = verbose;
+        this.explain = explain;
+        this.encoding = encoding;
+    }
+
+    public SolverConfiguration(String objective, String timeout,
+            boolean verbose, boolean explain) {
+        this(objective, timeout, verbose, explain, false);
+    }
 }
