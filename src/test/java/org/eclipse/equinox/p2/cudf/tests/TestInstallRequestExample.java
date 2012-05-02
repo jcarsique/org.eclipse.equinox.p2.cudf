42104d093add41aad656e55c9b394eb5cbb559ec
Julien Carsique <jcarsique@nuxeo.com>
2012-05-02 17:40:30 +0200

    format and fix trendy obj

--- src/test/java/org/eclipse/equinox/p2/cudf/tests/TestInstallRequestExample.java
+++ src/test/java/org/eclipse/equinox/p2/cudf/tests/TestInstallRequestExample.java
@@ -9,53 +9,57 @@ import org.eclipse.equinox.p2.cudf.solver.*;
 
 //DISABLED - Made the class abstract to have the tests passing
 public abstract class TestInstallRequestExample extends TestCase {
-	private ProfileChangeRequest pcr = null;
+    private ProfileChangeRequest pcr = null;
 
-	protected void setUp() throws Exception {
-		pcr = new Parser().parse(this.getClass().getClassLoader().getResource("testData/instances/expectedSuccess/installRequest.cudf").openStream());
-	}
+    protected void setUp() throws Exception {
+        pcr = new Parser().parse(this.getClass().getClassLoader().getResource(
+                "testData/instances/expectedSuccess/installRequest.cudf").openStream());
+    }
 
-	public void testParanoid() {
-		SolverConfiguration configuration = new SolverConfiguration("paranoid", "1000c", true, false);
-		Object result = new SimplePlanner().getSolutionFor(pcr, configuration);
-		if (result instanceof Collection) {
-			Collection col = (Collection) result;
-			assertEquals(col.toString(), 2, col.size());
-		} else {
-			fail("No result found!");
-		}
-	}
+    public void testParanoid() {
+        SolverConfiguration configuration = new SolverConfiguration("paranoid",
+                "1000c", true, false);
+        Object result = new SimplePlanner().getSolutionFor(pcr, configuration);
+        if (result instanceof Collection) {
+            Collection col = (Collection) result;
+            assertEquals(col.toString(), 2, col.size());
+        } else {
+            fail("No result found!");
+        }
+    }
 
-	public void testP2() {
-		SolverConfiguration configuration = new SolverConfiguration("p2", "1000c", true, false);
-		Object result = new SimplePlanner().getSolutionFor(pcr, configuration);
-		if (result instanceof Collection) {
-			Collection col = (Collection) result;
-			assertEquals(col.toString(), 3, col.size());
-		} else {
-			fail("No result found!");
-		}
-	}
+    public void testP2() {
+        SolverConfiguration configuration = new SolverConfiguration("p2",
+                "1000c", true, false);
+        Object result = new SimplePlanner().getSolutionFor(pcr, configuration);
+        if (result instanceof Collection) {
+            Collection col = (Collection) result;
+            assertEquals(col.toString(), 3, col.size());
+        } else {
+            fail("No result found!");
+        }
+    }
 
-	public void testTrendy() {
-		SolverConfiguration configuration = new SolverConfiguration("trndy", "1000c", true, false);
-		Object result = new SimplePlanner().getSolutionFor(pcr, configuration);
-		if (result instanceof Collection) {
-			Collection col = (Collection) result;
-			assertEquals(col.toString(), 2, col.size());
-		} else {
-			fail("No result found!");
-		}
-	}
+    public void testTrendy() {
+        SolverConfiguration configuration = new SolverConfiguration("trendy",
+                "1000c", true, false);
+        Object result = new SimplePlanner().getSolutionFor(pcr, configuration);
+        if (result instanceof Collection) {
+            Collection col = (Collection) result;
+            assertEquals(col.toString(), 2, col.size());
+        } else {
+            fail("No result found!");
+        }
+    }
 
-	private InstallableUnit getIU(Collection col, String id) {
-		Iterator it = col.iterator();
-		while (it.hasNext()) {
-			InstallableUnit iu = (InstallableUnit) it.next();
-			if (id.equals(iu.getId()))
-				return iu;
-		}
-		fail("Can't find: " + id);
-		return null;
-	}
+    private InstallableUnit getIU(Collection col, String id) {
+        Iterator it = col.iterator();
+        while (it.hasNext()) {
+            InstallableUnit iu = (InstallableUnit) it.next();
+            if (id.equals(iu.getId()))
+                return iu;
+        }
+        fail("Can't find: " + id);
+        return null;
+    }
 }
