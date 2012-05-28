065f38908b175b741d131471f942ba28af521e9b
Julien Carsique <jcarsique@nuxeo.com>
2012-05-28 17:55:15 +0200

    test script

---
+++ test.sh
@@ -0,0 +1,1 @@
+java -jar target/org.eclipse.equinox.p2.cudf-1.15-NX-SNAPSHOT-jar-with-dependencies.jar -verbose -explain -obj -removed,-notuptodate,-unsat_recommends,-new,-changed,-versionchanged  /tmp/request.cudf
