e86572c60e35306b79fe4219c3f2f5fd978d5aae
Julien Carsique <jcarsique@nuxeo.com>
2012-10-12 18:49:43 +0200

    improved test script

--- test.sh
+++ test.sh
@@ -1,1 +1,13 @@
-java -jar target/org.eclipse.equinox.p2.cudf-1.15-NX-SNAPSHOT-jar-with-dependencies.jar -verbose -explain -obj -removed,-notuptodate,-unsat_recommends,-new,-changed,-versionchanged  /tmp/request.cudf
+REQUEST=${1:-/tmp/request.cudf}
+echo REQUEST: $REQUEST
+CRITERIA=${2:-"-removed,-notuptodate,-unsat_recommends,-new,-changed,-versionchanged"}
+echo CRITERIA: $CRITERIA
+echo
+java -jar `ls target/org.eclipse.equinox.p2.cudf*jar-with-dependencies.jar` -obj $CRITERIA $REQUEST
+
+#echo WITH VERBOSE:
+#java -jar `ls target/org.eclipse.equinox.p2.cudf*jar-with-dependencies.jar` -verbose -obj $CRITERIA $REQUEST
+
+# BUG: verbose+explain changes the results!
+#echo WITH VERBOSE+EXPLAIN:
+#java -jar `ls target/org.eclipse.equinox.p2.cudf*jar-with-dependencies.jar` -verbose -explain -obj $CRITERIA $REQUEST
